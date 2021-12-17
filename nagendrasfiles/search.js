2b5c67ff60aaa01aa6e79113b4023912123b1161
nag-murali <93373775+nag-murali@users.noreply.github.com>
2021-12-17 21:39:47 +0530

    altered search

--- nagendrasfiles/search.js
+++ nagendrasfiles/search.js
@@ -5,3 +5,20 @@ let redirecthome = () => {
 };
 
 document.getElementById("crossbtn").addEventListener("click", redirecthome);
+
+let Toitem = (e) => {
+ 
+    let value = document.querySelector("input").value;
+    value = value.toUpperCase();
+    console.log(value);
+   if(e.key === "Enter"){
+     
+    if(value == "DUVET COVERS"){
+
+       window.location.href = "itempage.html"
+    }
+    
+   }
+
+   
+}
